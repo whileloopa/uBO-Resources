d3e87f3534c1e7db5496289b892343f5c3b394a5
whileloopa <towlyhk-github@yahoo.com.hk>
2020-04-30 19:58:19 +0800

    Add files via upload

--- resources_my.js
+++ resources_my.js
@@ -9,6 +9,9 @@
 
     https://github.com/uBlock-user/uBO-Scriptlets/blob/master/scriptlets.js
     https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.js
+    
+    https://github.com/whileloopa/uBO-Scriptlets/blob/master/resources_my.js
+    https://raw.githubusercontent.com/whileloopa/uBO-Scriptlets/master/resources_my.js
 
     https://www.dropbox.com/s/rqnyd24mgwpm31i/resources_my.js?dl=0
     https://dl.dropboxusercontent.com/s/rqnyd24mgwpm31i/resources_my.js
